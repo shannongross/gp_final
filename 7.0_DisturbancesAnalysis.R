6415fad2d581cc68a686798d09ef72b9033d52ff
shannongross <36520472+shannongross@users.noreply.github.com>
2024-04-12 11:51:25 -0400

    disturbances analysis

--- 7.0_DisturbancesAnalysis.R
+++ 7.0_DisturbancesAnalysis.R
@@ -22,6 +22,8 @@ chosen_model <- "NoGIS_Unstrat"
 chosen_version <- "V4"
 parent_path <- paste0(HOME_DIR, "/output/models/", model_version,
                       "/", chosen_model,"/", chosen_version, "_", chosen_model)
+disturb_path <- paste0(parent_path, "/disturb_analysis")
+if (!dir.exists(disturb_path)){dir.create(disturb_path)}
 
 # Get results dataset 
 df_results <- read_csv(paste0(parent_path, "/refined_results.csv"))
@@ -60,8 +62,6 @@ df_disturbed$SampleWasDisturbed <- as.numeric(df_disturbed$SampleWasDisturbed)
 number_disturbed <- sum(df_disturbed$SampleWasDisturbed)
 print(paste("number with at least one flag in", chosen_model, chosen_version, "model:", number_disturbed))
 
-write.csv(df_disturbed, paste0(parent_path,"/df_lu_disturbances.csv"))
-
 summary_disturb <- df_disturbed %>% group_by(SampleWasDisturbed) %>%
   summarise(n_samples=length(Region),
             n_Training=sum(Dataset=="Training"),
@@ -74,8 +74,121 @@ summary_disturb <- df_disturbed %>% group_by(SampleWasDisturbed) %>%
             pctCorrect_PvIvE_CorrectClass50=n_correct_PvIvE_CorrectClass50/n_samples,
             n_correct_EvALI_CorrectClass50=sum(EvALI_correct),
             pctCorrect_EvALI_CorrectClass50=n_correct_EvALI_CorrectClass50/n_samples
-  ) 
-summary_disturb <- t(summary_disturb)
+  ) %>% mutate(
+    SampleWasDisturbed=case_when(SampleWasDisturbed==0~"NotDisturbed",
+                                 SampleWasDisturbed==1~"Disturbed"))
+# summary_disturb <- t(summary_disturb)
 print(summary_disturb)
 
-write.csv(summary_disturb, paste0(parent_path,"/summary_disturb.csv"))
+df_disturbed <- df_disturbed %>% mutate(
+  SampleWasDisturbed=case_when(SampleWasDisturbed==0~"NotDisturbed",
+                               SampleWasDisturbed==1~"Disturbed"),
+  TypeOfDisturbance=case_when(agricultural==1 & urban==0 ~"agricultural",
+                              urban==1 & agricultural==0 ~"urban",
+                              urban==1 & agricultural==1 ~"ag and urban",
+                              urban==0 & agricultural==0 ~"none",
+  ))
+
+
+write.csv(df_disturbed, paste0(disturb_path,"/df_lu_disturbances.csv"))
+
+
+write.csv(summary_disturb, paste0(disturb_path,"/summary_disturb.csv"))
+
+
+plot_disturbed_samples <- ggplot(data=df_disturbed, 
+                                 aes(x=Region_detail,  
+                                     fill=TypeOfDisturbance)) +
+  geom_bar(stat="count", position=position_dodge()) +
+  facet_wrap(~Dataset) +
+  labs(title = "Disturbances Analysis",
+       # subtitle = "(Before train-test split or oversampling)", 
+       y="",
+       x=""
+  ) 
+print(plot_disturbed_samples)
+ggsave(plot_disturbed_samples, 
+          filename=paste0(disturb_path, "/disturb_by_subregion.png"),
+          dpi=300, height=5, width=9)
+
+
+
+plot_disturbed_samples <- ggplot(data=df_disturbed, 
+                                 aes(x=Region_detail,  
+                                     fill=TypeOfDisturbance)) +
+  geom_bar(stat="count", position=position_dodge(), width = 0.6) +
+  scale_fill_manual(values=c("ag and urban"="#bd2828", 
+                             "agricultural"="#918d1d", 
+                             "urban"="#2b98ab", 
+                             "none"="#757574"
+                            ))  +
+  labs(title = "Disturbances Analysis",
+       # subtitle = "(Before train-test split or oversampling)", 
+       y="",
+       x=""
+  ) 
+print(plot_disturbed_samples)
+ggsave(plot_disturbed_samples, 
+       filename=paste0(disturb_path, "/disturb_by_type.png"),
+       dpi=300, height=5, width=9)
+
+
+
+
+
+
+
+plot_disturbed_pvive <- ggplot(data=summary_disturb, 
+       aes(x=SampleWasDisturbed, 
+           y=pctCorrect_PvIvE_CorrectClass50,
+           fill=SampleWasDisturbed)) +
+  geom_bar(stat="identity") +
+  labs(title = "Disturbances Analysis",
+       subtitle = "PvIvE",
+       y="% PvIvE Accuracy",
+       x=""
+  ) + ylim(0,1)
+print(plot_disturbed_pvive)   
+ggsave(plot_disturbed_pvive, 
+       filename=paste0(disturb_path, "/disturb_pvive.png"),
+       dpi=300, height=5, width=6)
+
+plot_disturbed_evali <- ggplot(data=summary_disturb, 
+                               aes(x=SampleWasDisturbed, 
+                                   y=pctCorrect_EvALI_CorrectClass50, 
+                                   fill=SampleWasDisturbed )) +
+  geom_bar(stat="identity") +
+  labs(title = "Disturbances Analysis",
+       subtitle = "EvALI",
+       y="% EvALI Accuracy",
+       x=""
+  ) +
+  # facet_wrap(~Datset) + 
+  ylim(0,1)
+print(plot_disturbed_evali)   
+ggsave(plot_disturbed_evali, 
+       filename=paste0(disturb_path, "/disturb_evali.png"),
+       dpi=300, height=5, width=6)
+
+
+       
+plot_disturbed_summary <- ggplot(data=summary_disturb, 
+                                 aes(x=Region_detail,  
+                                     fill=TypeOfDisturbance)) +
+  geom_bar(stat="count", position=position_dodge(), width = 0.6) +
+  scale_fill_manual(values=c("ag and urban"="#bd2828", 
+                             "agricultural"="#918d1d", 
+                             "urban"="#2b98ab", 
+                             "none"="#757574"
+  ))  +
+  labs(title = "Disturbances Analysis",
+       # subtitle = "(Before train-test split or oversampling)", 
+       y="",
+       x=""
+  ) 
+print(plot_disturbed_summary)
+ggsave(plot_disturbed_summary, 
+       filename=paste0(disturb_path, "/disturb_summary.png"),
+       dpi=300, height=5, width=9)
+
+
--- ReadMe.md
+++ ReadMe.md
@@ -2,7 +2,7 @@
 
 0_PrepData.R: This script formats the data provided by EPA. Handles NaNs, checks for outliers, splits into training/testing, performs data augmentation (upsampling), etc.
 
-1.0_Screening.R: This scripts checks if any of the candidate metrics are too homogenous to be considered further in the analysis (ie, if data are all zeros, it will be dropped here)
+1.0_Screening.R: This scripts checks if any of the candidate metrics are too homogeneous to be considered further in the analysis (ie, if data are all zeros, it will be dropped here)
 
 2.0_PrelimModels.R: This is the main script that generates the random forest model. Depending on what case it is, there will be some nuances (ie the GP_S no GIS model should only allow samples from the South, and no GIS metrics should be included)
 
@@ -14,6 +14,8 @@ step.
 5.0_SingleIndicators.R:
 
 6.0_SensitivityAnalysis.R : IN PROGRESS
+
+7.0_DisturbancesAnalysis.R : IN PROGRESS
 
 Final_Model_forWill.R: a simple script to reproduce/restore the final GP model 
 results. For use in web app development.
